b5a591844fe807d11da669e4d3bd79d0c523599e
Vladimir Afashokov <vladimir.afashokov@ru.axxonsoft.com>
2017-10-24 15:22:42 +0300

    []

--- pikuli/_functions.py
+++ pikuli/_functions.py
@@ -74,9 +74,11 @@ def wait_while_not(f_logic, timeout, warning_timeout=None):
 def addImagePath(path):
     pikuli.Settings.addImagePath(path)
 
+def get_hwnd_by_location(x, y):
+    return win32gui.WindowFromPoint((x, y))
+
 def setFindFailedDir(path):
     pikuli.Settings.setFindFailedDir(path)
-
 
 def _monitor_hndl_to_screen_n(m_hndl):
     ''' Экраны-мониторы нуменруются от 1. Нулевой экран -- это полный вирутальный. '''
