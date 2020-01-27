788758e1f3dd12139676576b946f3c65ca20ff93
Arsen Shogenov <arsen.shogenov@ru.axxonsoft.com>
2020-01-27 10:52:57 +0300

    [ROBO-5327]

--- pikuli/input/__init__.py
+++ pikuli/input/__init__.py
@@ -5,8 +5,32 @@ TODO: see
     https://github.com/moses-palmer/pynput
     https://github.com/asweigart/pyautogui
 """
+import traceback
 
-from .input_emulator import InputEmulator
-from .keys import Key, KeyModifier, ScrollDirection
-from .platform_init import Clipboard, ButtonCode
+from pikuli._helpers import NotImplemetedDummyFactory
+
+try:
+    from .input_emulator import InputEmulator
+    from .keys import Key, KeyModifier, ScrollDirection
+    from .platform_init import Clipboard, ButtonCode
+
+except Exception as ex:
+    err_msg = traceback.format_exc()
+    (
+        InputEmulator,
+        Key,
+        KeyModifier,
+        ScrollDirection,
+        Clipboard,
+        ButtonCode
+    ) = NotImplemetedDummyFactory.make_classes(
+        [
+            'InputEmulator',
+            'Key',
+            'KeyModifier',
+            'ScrollDirection',
+            'Clipboard',
+            'ButtonCode',
+        ],
+        reason=err_msg)
 
